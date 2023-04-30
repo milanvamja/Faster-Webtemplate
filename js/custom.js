118a2654bf879b56ed8503ad84dd37b295e1dc0b
milan vamja <129034172+milanvamja@users.noreply.github.com>
2023-04-30 17:28:44 +0530

    new upload

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+    $("#slider_1").owlCarousel({
+        loop:true,
+        margin:30,
+        nav:false,
+        items:3,
+        dots:true,
+        dotsEach:true,
+        center:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+                
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+
+    })
+    
+  });
